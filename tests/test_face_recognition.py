364111ba21ebf67347cff96838d465a1bf80c85e
Marcus Medley <mdmedley@users.noreply.github.com>
2017-03-16 14:45:41 -0700

    Refactor Tests (#8)
    * Refactor Tests

--- tests/test_face_recognition.py
+++ tests/test_face_recognition.py
@@ -20,65 +20,73 @@ from face_recognition import cli
 class Test_face_recognition(unittest.TestCase):
 
     def test_load_image_file(self):
-        img = api.load_image_file(os.path.join(os.path.dirname(__file__), "test_images", "obama.jpg"))
-        assert img.shape == (1137, 910, 3)
+        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
+        self.assertEqual(img.shape, (1137, 910, 3))
 
     def test_load_image_file_32bit(self):
-        img = api.load_image_file(os.path.join(os.path.dirname(__file__), "test_images", "32bit.png"))
-        assert img.shape == (1200, 626, 3)
+        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', '32bit.png'))
+        self.assertEqual(img.shape, (1200, 626, 3))
 
     def test_raw_face_locations(self):
-        img = api.load_image_file(os.path.join(os.path.dirname(__file__), "test_images", "obama.jpg"))
+        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
         detected_faces = api._raw_face_locations(img)
 
-        assert len(detected_faces) == 1
-        assert detected_faces[0].top() == 142
-        assert detected_faces[0].bottom() == 409
+        self.assertEqual(len(detected_faces), 1)
+        self.assertEqual(detected_faces[0].top(), 142)
+        self.assertEqual(detected_faces[0].bottom(), 409)
 
     def test_raw_face_locations_32bit_image(self):
-        img = api.load_image_file(os.path.join(os.path.dirname(__file__), "test_images", "32bit.png"))
+        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', '32bit.png'))
         detected_faces = api._raw_face_locations(img)
 
-        assert len(detected_faces) == 1
-        assert detected_faces[0].top() == 290
-        assert detected_faces[0].bottom() == 558
+        self.assertEqual(len(detected_faces), 1)
+        self.assertEqual(detected_faces[0].top(), 290)
+        self.assertEqual(detected_faces[0].bottom(), 558)
 
     def test_face_locations(self):
-        img = api.load_image_file(os.path.join(os.path.dirname(__file__), "test_images", "obama.jpg"))
+        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
         detected_faces = api.face_locations(img)
 
-        assert len(detected_faces) == 1
-        assert detected_faces[0] == (142, 617, 409, 349)
+        self.assertEqual(len(detected_faces), 1)
+        self.assertEqual(detected_faces[0], (142, 617, 409, 349))
 
     def test_raw_face_landmarks(self):
-        img = api.load_image_file(os.path.join(os.path.dirname(__file__), "test_images", "obama.jpg"))
+        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
         face_landmarks = api._raw_face_landmarks(img)
         example_landmark = face_landmarks[0].parts()[10]
 
-        assert len(face_landmarks) == 1
-        assert face_landmarks[0].num_parts == 68
-        assert (example_landmark.x, example_landmark.y) == (552, 399)
+        self.assertEqual(len(face_landmarks), 1)
+        self.assertEqual(face_landmarks[0].num_parts, 68)
+        self.assertEqual((example_landmark.x, example_landmark.y), (552, 399))
 
     def test_face_landmarks(self):
-        img = api.load_image_file(os.path.join(os.path.dirname(__file__), "test_images", "obama.jpg"))
+        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
         face_landmarks = api.face_landmarks(img)
 
-        assert set(face_landmarks[0].keys()) == set(['chin', 'left_eyebrow', 'right_eyebrow', 'nose_bridge', 'nose_tip', 'left_eye', 'right_eye', 'top_lip', 'bottom_lip'])
-        assert face_landmarks[0]['chin'] == [(369, 220), (372, 254), (378, 289), (384, 322), (395, 353), (414, 382), (437, 407), (464, 424), (495, 428), (527, 420), (552, 399), (576, 372), (594, 344), (604, 314)]
+        self.assertEqual(
+            set(face_landmarks[0].keys()),
+            set(['chin', 'left_eyebrow', 'right_eyebrow', 'nose_bridge',
+                 'nose_tip', 'left_eye', 'right_eye', 'top_lip',
+                 'bottom_lip']))
+        self.assertEqual(
+            face_landmarks[0]['chin'],
+            [(369, 220), (372, 254), (378, 289), (384, 322), (395, 353),
+             (414, 382), (437, 407), (464, 424), (495, 428), (527, 420),
+             (552, 399), (576, 372), (594, 344), (604, 314)])
 
     def test_face_encodings(self):
-        img = api.load_image_file(os.path.join(os.path.dirname(__file__), "test_images", "obama.jpg"))
+        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
         encodings = api.face_encodings(img)
 
-        assert len(encodings) == 1
-        assert len(encodings[0]) == 128
+        self.assertEqual(len(encodings), 1)
+        self.assertEqual(len(encodings[0]), 128)
 
     def test_compare_faces(self):
-        img_a1 = api.load_image_file(os.path.join(os.path.dirname(__file__), "test_images", "obama.jpg"))
-        img_a2 = api.load_image_file(os.path.join(os.path.dirname(__file__), "test_images", "obama2.jpg"))
-        img_a3 = api.load_image_file(os.path.join(os.path.dirname(__file__), "test_images", "obama3.jpg"))
+        img_a1 = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
+        img_a2 = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama2.jpg'))
+        img_a3 = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama3.jpg'))
 
-        img_b1 = api.load_image_file(os.path.join(os.path.dirname(__file__), "test_images", "biden.jpg"))
+        img_b1 = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'biden.jpg'))
 
         face_encoding_a1 = api.face_encodings(img_a1)[0]
         face_encoding_a2 = api.face_encodings(img_a2)[0]
@@ -88,16 +96,16 @@ class Test_face_recognition(unittest.TestCase):
         faces_to_compare = [
             face_encoding_a2,
             face_encoding_a3,
-            face_encoding_b1
-        ]
+            face_encoding_b1]
 
         match_results = api.compare_faces(faces_to_compare, face_encoding_a1)
-        assert match_results[0] == True
-        assert match_results[1] == True
-        assert match_results[2] == False
+        self.assertIs(match_results[0], True)
+        self.assertIs(match_results[1], True)
+        self.assertIs(match_results[2], False)
 
     def test_command_line_interface(self):
+        target_string = '--help  Show this message and exit.'
         runner = CliRunner()
         help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        self.assertEqual(help_result.exit_code, 0)
+        self.assertTrue(target_string in help_result.output)
