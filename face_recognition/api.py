3f1a47d37075071d6df9c060d00d8a73ce74740b
Adam Geitgey <ageitgey@gmail.com>
2017-04-04 11:56:33 -0700

    Expose face_distance and prevent errors with empty inputs

--- face_recognition/api.py
+++ face_recognition/api.py
@@ -52,16 +52,19 @@ def _trim_css_to_bounds(css, image_shape):
     return max(css[0], 0), min(css[1], image_shape[1]), min(css[2], image_shape[0]), max(css[3], 0)
 
 
-def _face_distance(faces, face_to_compare):
+def face_distance(face_encodings, face_to_compare):
     """
-    Given a list of face encodings, compared them to a known face encoding and get a euclidean distance
-    for each comparison face.
+    Given a list of face encodings, compare them to a known face encoding and get a euclidean distance
+    for each comparison face. The distance tells you how similar the faces are.
 
     :param faces: List of face encodings to compare
     :param face_to_compare: A face encoding to compare against
-    :return: A list with the distance for each face in the same order as the 'faces' array
+    :return: A numpy ndarray with the distance for each face in the same order as the 'faces' array
     """
-    return np.linalg.norm(faces - face_to_compare, axis=1)
+    if len(face_encodings) == 0:
+        return np.empty((0))
+
+    return np.linalg.norm(face_encodings - face_to_compare, axis=1)
 
 
 def load_image_file(filename, mode='RGB'):
@@ -154,4 +157,4 @@ def compare_faces(known_face_encodings, face_encoding_to_check, tolerance=0.6):
     :param tolerance: How much distance between faces to consider it a match. Lower is more strict. 0.6 is typical best performance.
     :return: A list of True/False values indicating which known_face_encodings match the face encoding to check
     """
-    return list(_face_distance(known_face_encodings, face_encoding_to_check) <= tolerance)
+    return list(face_distance(known_face_encodings, face_encoding_to_check) <= tolerance)
