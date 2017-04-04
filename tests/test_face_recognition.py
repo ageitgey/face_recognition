8f8c8b15a46488175356793898f3b1b7452f9028
Adam Geitgey <ageitgey@gmail.com>
2017-04-04 11:56:33 -0700

    Expose face_distance and prevent errors with empty inputs

--- tests/test_face_recognition.py
+++ tests/test_face_recognition.py
@@ -11,6 +11,7 @@ Tests for `face_recognition` module.
 
 import unittest
 import os
+import numpy as np
 from click.testing import CliRunner
 
 from face_recognition import api
@@ -94,6 +95,50 @@ class Test_face_recognition(unittest.TestCase):
         self.assertEqual(len(encodings), 1)
         self.assertEqual(len(encodings[0]), 128)
 
+    def test_face_distance(self):
+        img_a1 = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
+        img_a2 = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama2.jpg'))
+        img_a3 = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama3.jpg'))
+
+        img_b1 = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'biden.jpg'))
+
+        face_encoding_a1 = api.face_encodings(img_a1)[0]
+        face_encoding_a2 = api.face_encodings(img_a2)[0]
+        face_encoding_a3 = api.face_encodings(img_a3)[0]
+        face_encoding_b1 = api.face_encodings(img_b1)[0]
+
+        faces_to_compare = [
+            face_encoding_a2,
+            face_encoding_a3,
+            face_encoding_b1]
+
+        distance_results = api.face_distance(faces_to_compare, face_encoding_a1)
+
+        # 0.6 is the default face distance match threshold. So we'll spot-check that the numbers returned
+        # are above or below that based on if they should match (since the exact numbers could vary).
+        self.assertEqual(type(distance_results), np.ndarray)
+        self.assertLessEqual(distance_results[0], 0.6)
+        self.assertLessEqual(distance_results[1], 0.6)
+        self.assertGreater(distance_results[2], 0.6)
+
+    def test_face_distance_empty_lists(self):
+        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'biden.jpg'))
+        face_encoding = api.face_encodings(img)[0]
+
+        # empty python list
+        faces_to_compare = []
+
+        distance_results = api.face_distance(faces_to_compare, face_encoding)
+        self.assertEqual(type(distance_results), np.ndarray)
+        self.assertEqual(len(distance_results), 0)
+
+        # empty numpy list
+        faces_to_compare = np.array([])
+
+        distance_results = api.face_distance(faces_to_compare, face_encoding)
+        self.assertEqual(type(distance_results), np.ndarray)
+        self.assertEqual(len(distance_results), 0)
+
     def test_compare_faces(self):
         img_a1 = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
         img_a2 = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama2.jpg'))
@@ -112,9 +157,29 @@ class Test_face_recognition(unittest.TestCase):
             face_encoding_b1]
 
         match_results = api.compare_faces(faces_to_compare, face_encoding_a1)
+
+        self.assertEqual(type(match_results), list)
         self.assertTrue(match_results[0])
         self.assertTrue(match_results[1])
         self.assertFalse(match_results[2])
+
+    def test_compare_faces_empty_lists(self):
+        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'biden.jpg'))
+        face_encoding = api.face_encodings(img)[0]
+
+        # empty python list
+        faces_to_compare = []
+
+        match_results = api.compare_faces(faces_to_compare, face_encoding)
+        self.assertEqual(type(match_results), list)
+        self.assertListEqual(match_results, [])
+
+        # empty numpy list
+        faces_to_compare = np.array([])
+
+        match_results = api.compare_faces(faces_to_compare, face_encoding)
+        self.assertEqual(type(match_results), list)
+        self.assertListEqual(match_results, [])
 
     def test_command_line_interface(self):
         target_string = '--help  Show this message and exit.'
