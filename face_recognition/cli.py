b8a803731c23dbf73ac3e219a847f6e2ea5a582d
Adam Geitgey <ageitgey@gmail.com>
2017-08-03 10:31:49 -0700

    Add tolerance parameter to CLI

--- face_recognition/cli.py
+++ face_recognition/cli.py
@@ -32,7 +32,7 @@ def scan_known_people(known_people_folder):
     return known_names, known_face_encodings
 
 
-def test_image(image_to_check, known_names, known_face_encodings):
+def test_image(image_to_check, known_names, known_face_encodings, tolerance=0.6):
     unknown_image = face_recognition.load_image_file(image_to_check)
 
     # Scale down image if it's giant so things run a little faster
@@ -45,7 +45,7 @@ def test_image(image_to_check, known_names, known_face_encodings):
     unknown_encodings = face_recognition.face_encodings(unknown_image)
 
     for unknown_encoding in unknown_encodings:
-        result = face_recognition.compare_faces(known_face_encodings, unknown_encoding)
+        result = face_recognition.compare_faces(known_face_encodings, unknown_encoding, tolerance=tolerance)
 
         if True in result:
             [print("{},{}".format(image_to_check, name)) for is_match, name in zip(result, known_names) if is_match]
@@ -57,7 +57,7 @@ def image_files_in_folder(folder):
     return [os.path.join(folder, f) for f in os.listdir(folder) if re.match(r'.*\.(jpg|jpeg|png)', f, flags=re.I)]
 
 
-def process_images_in_process_pool(images_to_check, known_names, known_face_encodings, number_of_cpus):
+def process_images_in_process_pool(images_to_check, known_names, known_face_encodings, number_of_cpus, tolerance):
     if number_of_cpus == -1:
         processes = None
     else:
@@ -69,7 +69,7 @@ def process_images_in_process_pool(images_to_check, known_names, known_face_enco
         context = multiprocessing.get_context("forkserver")
 
     pool = context.Pool(processes=processes)
-    function_parameters = zip(images_to_check, itertools.repeat(known_names), itertools.repeat(known_face_encodings))
+    function_parameters = zip(images_to_check, itertools.repeat(known_names), itertools.repeat(known_face_encodings),  itertools.repeat(tolerance))
 
     pool.starmap(test_image, function_parameters)
 
@@ -78,7 +78,8 @@ def process_images_in_process_pool(images_to_check, known_names, known_face_enco
 @click.argument('known_people_folder')
 @click.argument('image_to_check')
 @click.option('--cpus', default=1, help='number of CPU cores to use in parallel (can speed up processing lots of images). -1 means "use all in system"')
-def main(known_people_folder, image_to_check, cpus):
+@click.option('--tolerance', default=0.6, help='Tolerance to use for face comparisons. Default is 0.6 and lower is more strict matching. Lower this if you get multiple matches for the same person. Try 0.54 for example.')
+def main(known_people_folder, image_to_check, cpus, tolerance):
     known_names, known_face_encodings = scan_known_people(known_people_folder)
 
     # Multi-core processing only supported on Python 3.4 or greater
@@ -88,9 +89,9 @@ def main(known_people_folder, image_to_check, cpus):
 
     if os.path.isdir(image_to_check):
         if cpus == 1:
-            [test_image(image_file, known_names, known_face_encodings) for image_file in image_files_in_folder(image_to_check)]
+            [test_image(image_file, known_names, known_face_encodings, tolerance) for image_file in image_files_in_folder(image_to_check)]
         else:
-            process_images_in_process_pool(image_files_in_folder(image_to_check), known_names, known_face_encodings, cpus)
+            process_images_in_process_pool(image_files_in_folder(image_to_check), known_names, known_face_encodings, cpus, tolerance)
     else:
         test_image(image_to_check, known_names, known_face_encodings)
 
