b4b686d5bcccf0def93b8cd05bd1d12226ee9c09
Itamar Shenhar <itamar8910@gmail.com>
2017-12-02 15:43:37 +0200

    fixed paths in test

--- tests/test_face_recognition_knn.py
+++ tests/test_face_recognition_knn.py
@@ -13,40 +13,40 @@ class Test_face_recognition(unittest.TestCase):
     def test_knn1(self):
         knn_clf = face_recognition_knn.train("test_images_knn/train")
 
-        assert("obama" in face_recognition_knn.predict(join("test_images_knn/test", "obama1.jpg"), knn_clf=knn_clf)[0])
+        assert("obama" in face_recognition_knn.predict(join(os.path.dirname(__file__), "test_images_knn/test", "obama1.jpg"), knn_clf=knn_clf)[0])
 
-        assert("alex_lacamoire" in face_recognition_knn.predict(join("test_images_knn/test", "alex_lacamoire1.jpg"), knn_clf=knn_clf)[0])
+        assert("alex_lacamoire" in face_recognition_knn.predict(join(os.path.dirname(__file__), "test_images_knn/test", "alex_lacamoire1.jpg"), knn_clf=knn_clf)[0])
 
-        assert("kit_harington" in face_recognition_knn.predict(join("test_images_knn/test", "johnsnow_test1.jpg"), knn_clf=knn_clf)[0])
+        assert("kit_harington" in face_recognition_knn.predict(join(os.path.dirname(__file__), "test_images_knn/test", "johnsnow_test1.jpg"), knn_clf=knn_clf)[0])
 
         assert(sorted(["kit_harington", "rose_leslie"]) == sorted([pred[0] for pred in
                                                                    face_recognition_knn.predict(
-                                                                       join("test_images_knn/test", "kit_with_rose.jpg"),
+                                                                       join(os.path.dirname(__file__), "test_images_knn/test", "kit_with_rose.jpg"),
                                                                        knn_clf=knn_clf)]))
 
         assert(sorted(["obama", "biden", "N/A"]) == sorted([pred[0] for pred in
                                                             face_recognition_knn.predict(
-                                                                join("test_images_knn/test", "obama_and_biden.jpg"),
+                                                                join(os.path.dirname(__file__), "test_images_knn/test", "obama_and_biden.jpg"),
                                                                 knn_clf=knn_clf)]))
 
     def test_knn_pickle(self):
-        pickle_path = "test_images_knn/knn_clf.p"
+        pickle_path = join(os.path.dirname(__file__), "test_images_knn/knn_clf.p")
         face_recognition_knn.train("test_images_knn/train", model_save_path=pickle_path)
 
         try:
-            assert("obama" in face_recognition_knn.predict(join("test_images_knn/test", "obama1.jpg"), model_save_path=pickle_path)[0])
+            assert("obama" in face_recognition_knn.predict(join(os.path.dirname(__file__), "test_images_knn/test", "obama1.jpg"), model_save_path=pickle_path)[0])
 
-            assert("alex_lacamoire" in face_recognition_knn.predict(join("test_images_knn/test", "alex_lacamoire1.jpg"), model_save_path=pickle_path)[0])
+            assert("alex_lacamoire" in face_recognition_knn.predict(join(os.path.dirname(__file__), "test_images_knn/test", "alex_lacamoire1.jpg"), model_save_path=pickle_path)[0])
 
-            assert("kit_harington" in face_recognition_knn.predict(join("test_images_knn/test", "johnsnow_test1.jpg"), model_save_path=pickle_path)[0])
+            assert("kit_harington" in face_recognition_knn.predict(join(os.path.dirname(__file__), "test_images_knn/test", "johnsnow_test1.jpg"), model_save_path=pickle_path)[0])
 
             assert(sorted(["kit_harington", "rose_leslie"]) == sorted([pred[0] for pred in face_recognition_knn.predict(
-                join("test_images_knn/test", "kit_with_rose.jpg"),
+                join(os.path.dirname(__file__), "test_images_knn/test", "kit_with_rose.jpg"),
                 model_save_path=pickle_path)]))
 
             assert(sorted(["obama", "biden", "N/A"]) == sorted([pred[0] for pred in
                                                                 face_recognition_knn.predict(
-                                                                    join("test_images_knn/test", "obama_and_biden.jpg"),
+                                                                    join(os.path.dirname(__file__), "test_images_knn/test", "obama_and_biden.jpg"),
                                                                     model_save_path=pickle_path)]))
         finally:
             if isfile(pickle_path):
