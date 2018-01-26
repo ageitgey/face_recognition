657e98bfa2f2b55da4449c8271a108bf4f193e05
Adam Geitgey <ageitgey@gmail.com>
2018-01-26 08:14:53 -0800

    Handle bad image files in example

--- examples/recognize_faces_in_pictures.py
+++ examples/recognize_faces_in_pictures.py
@@ -8,9 +8,13 @@ unknown_image = face_recognition.load_image_file("obama2.jpg")
 # Get the face encodings for each face in each image file
 # Since there could be more than one face in each image, it returns a list of encodings.
 # But since I know each image only has one face, I only care about the first encoding in each image, so I grab index 0.
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+try:
+    biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+    obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+    unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+except IndexError:
+    print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
+    quit()
 
 known_faces = [
     biden_face_encoding,
