28557a79017875a2ee41de896ccb36fce46f94c8
Adam Geitgey <ageitgey@gmail.com>
2018-02-01 11:51:30 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='face_recognition',
-    version='1.1.0',
+    version='1.2.0',
     description="Recognize faces from Python or from the command line",
     long_description=readme + '\n\n' + history,
     author="Adam Geitgey",
