699db80a5ff1e234221f91eb589fc7b89553b2d8
Xiaojian "JJ" Deng <xjd001@gmail.com>
2018-01-31 23:40:06 -0600

    Add blurb reminding the user to install Python 3.6 ver of Anaconda

--- setup_windows.bat
+++ setup_windows.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Make sure you've installed Anaconda: https://www.anaconda.com/download/
+echo Make sure you've installed the Python 3.6 version of Anaconda: https://www.anaconda.com/download/
 conda install -c conda-forge dlib numpy Pillow
 conda install scipy Click
 pip install -r requirements_windows.txt
