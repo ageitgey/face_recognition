949461ef94f6f34273fa8a918627ce7528e9e17e
Adam Geitgey <ageitgey@gmail.com>
2018-02-01 12:08:51 -0800

    Bump version to force a pypi refresh

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='face_recognition',
-    version='1.2.0',
+    version='1.2.1',
     description="Recognize faces from Python or from the command line",
     long_description=readme + '\n\n' + history,
     author="Adam Geitgey",
