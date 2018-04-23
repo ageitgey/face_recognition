eb472b597183c911667d59321a950e5b82b96054
xiang-zhe <36883691+xiang-zhe@users.noreply.github.com>
2018-04-23 11:29:11 +0800

    Update find_facial_features_in_picture.py
    '''
        pil_image = Image.fromarray(image)
        d = ImageDraw.Draw(pil_image)
    
    the two lines should be out of loop; otherwize, it only draws one face in multiple face image. 
    '''

--- examples/find_facial_features_in_picture.py
+++ examples/find_facial_features_in_picture.py
@@ -1,3 +1,10 @@
+'''
+    pil_image = Image.fromarray(image)
+    d = ImageDraw.Draw(pil_image)
+
+the two lines should be out of loop; otherwize, it only draws one face in multiple face image. 
+'''
+
 from PIL import Image, ImageDraw
 import face_recognition
 
@@ -9,10 +16,9 @@ face_landmarks_list = face_recognition.face_landmarks(image)
 
 print("I found {} face(s) in this photograph.".format(len(face_landmarks_list)))
 
-for face_landmarks in face_landmarks_list:
-
-    # Print the location of each facial feature in this image
-    facial_features = [
+pil_image = Image.fromarray(image)
+d = ImageDraw.Draw(pil_image)
+facial_features = [
         'chin',
         'left_eyebrow',
         'right_eyebrow',
@@ -23,13 +29,13 @@ for face_landmarks in face_landmarks_list:
         'top_lip',
         'bottom_lip'
     ]
+for face_landmarks in face_landmarks_list:
 
+    # Print the location of each facial feature in this image
     for facial_feature in facial_features:
         print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
 
     # Let's trace out each facial feature in the image with a line!
-    pil_image = Image.fromarray(image)
-    d = ImageDraw.Draw(pil_image)
 
     for facial_feature in facial_features:
         d.line(face_landmarks[facial_feature], width=5)
