631342b163c7a4ebcab6c1c7ed097af814cf3540
Guillaume 'Vermeille' Sanchez <guillaume.v.sanchez@gmail.com>
2018-04-24 19:29:28 +0200

    extract small landmarks

--- face_recognition/api.py
+++ face_recognition/api.py
@@ -162,7 +162,7 @@ def _raw_face_landmarks(face_image, face_locations=None, model="large"):
     return [pose_predictor(face_image, face_location) for face_location in face_locations]
 
 
-def face_landmarks(face_image, face_locations=None):
+def face_landmarks(face_image, face_locations=None, model="large"):
     """
     Given an image, returns a dict of face feature locations (eyes, nose, etc) for each face in the image
 
@@ -170,21 +170,30 @@ def face_landmarks(face_image, face_locations=None):
     :param face_locations: Optionally provide a list of face locations to check.
     :return: A list of dicts of face feature locations (eyes, nose, etc)
     """
-    landmarks = _raw_face_landmarks(face_image, face_locations)
+    landmarks = _raw_face_landmarks(face_image, face_locations, model)
     landmarks_as_tuples = [[(p.x, p.y) for p in landmark.parts()] for landmark in landmarks]
 
     # For a definition of each point index, see https://cdn-images-1.medium.com/max/1600/1*AbEg31EgkbXSQehuNJBlWg.png
-    return [{
-        "chin": points[0:17],
-        "left_eyebrow": points[17:22],
-        "right_eyebrow": points[22:27],
-        "nose_bridge": points[27:31],
-        "nose_tip": points[31:36],
-        "left_eye": points[36:42],
-        "right_eye": points[42:48],
-        "top_lip": points[48:55] + [points[64]] + [points[63]] + [points[62]] + [points[61]] + [points[60]],
-        "bottom_lip": points[54:60] + [points[48]] + [points[60]] + [points[67]] + [points[66]] + [points[65]] + [points[64]]
-    } for points in landmarks_as_tuples]
+    if model == 'large':
+        return [{
+            "chin": points[0:17],
+            "left_eyebrow": points[17:22],
+            "right_eyebrow": points[22:27],
+            "nose_bridge": points[27:31],
+            "nose_tip": points[31:36],
+            "left_eye": points[36:42],
+            "right_eye": points[42:48],
+            "top_lip": points[48:55] + [points[64]] + [points[63]] + [points[62]] + [points[61]] + [points[60]],
+            "bottom_lip": points[54:60] + [points[48]] + [points[60]] + [points[67]] + [points[66]] + [points[65]] + [points[64]]
+        } for points in landmarks_as_tuples]
+    elif model == 'small':
+        return [{
+            "nose_tip": [points[4]],
+            "left_eye": points[2:4],
+            "right_eye": points[0:2],
+        } for points in landmarks_as_tuples]
+    else:
+        raise ValueError("Invalid landmarks model type. Supported models are ['small', 'large'].")
 
 
 def face_encodings(face_image, known_face_locations=None, num_jitters=1):
