8a39b6ce3f097f2251adcb3485e7ee665b248b0c
khaerulumam42 <35139151+khaerulumam42@users.noreply.github.com>
2018-06-08 13:12:03 +0700

    Update facerec_from_webcam.py
    Update nearest distance between two encoded, sortir the nearsest distance for every detected face

--- examples/facerec_from_webcam.py
+++ examples/facerec_from_webcam.py
@@ -1,5 +1,6 @@
 import face_recognition
 import cv2
+import numpy as np
 
 # This is a super simple (but slow) example of running face recognition on live video from your webcam.
 # There's a second example that's a little more complicated but runs faster.
@@ -39,19 +40,22 @@ while True:
     # Find all the faces and face enqcodings in the frame of video
     face_locations = face_recognition.face_locations(rgb_frame)
     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-
+    
     # Loop through each face in this frame of video
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         # See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-
+        
         name = "Unknown"
-
+        
+        # Calculate face distance
+        face_distance = face_recognition.face_distance(known_face_encodings, face_encoding)
         # If a match was found in known_face_encodings, just use the first one.
         if True in matches:
             first_match_index = matches.index(True)
-            name = known_face_names[first_match_index]
-
+            # Sort nearest distance
+            name = known_face_names[np.argsort(face_distance)[0]]
+            
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
