71a6c45316a0f3c270743dd45b173653adf6ad7f
Adam Geitgey <ageitgey@gmail.com>
2018-08-21 09:48:31 -0700

    Tweak demo to show multiple faces in one window instead of separate windows

--- examples/find_facial_features_in_picture.py
+++ examples/find_facial_features_in_picture.py
@@ -2,12 +2,16 @@ from PIL import Image, ImageDraw
 import face_recognition
 
 # Load the jpg file into a numpy array
-image = face_recognition.load_image_file("biden.jpg")
+image = face_recognition.load_image_file("two_people.jpg")
 
 # Find all facial features in all the faces in the image
 face_landmarks_list = face_recognition.face_landmarks(image)
 
 print("I found {} face(s) in this photograph.".format(len(face_landmarks_list)))
+
+# Create a PIL imagedraw object so we can draw on the picture
+pil_image = Image.fromarray(image)
+d = ImageDraw.Draw(pil_image)
 
 for face_landmarks in face_landmarks_list:
 
@@ -16,10 +20,8 @@ for face_landmarks in face_landmarks_list:
         print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
 
     # Let's trace out each facial feature in the image with a line!
-    pil_image = Image.fromarray(image)
-    d = ImageDraw.Draw(pil_image)
-
     for facial_feature in face_landmarks.keys():
         d.line(face_landmarks[facial_feature], width=5)
 
-    pil_image.show()
+# Show the picture
+pil_image.show()
