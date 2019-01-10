a54c7de0baaea5dd85d39398bc358591f0980b9b
Jzhenli <42105688+Jzhenli@users.noreply.github.com>
2019-01-10 10:55:23 +0800

    modify the  parameters of face_encodings function
    make the  parameters more reasonable

--- face_recognition/api.py
+++ face_recognition/api.py
@@ -197,7 +197,7 @@ def face_landmarks(face_image, face_locations=None, model="large"):
         raise ValueError("Invalid landmarks model type. Supported models are ['small', 'large'].")
 
 
-def face_encodings(face_image, known_face_locations=None, num_jitters=1):
+def face_encodings(face_image, known_face_locations=None, num_jitters=1, model="small"):
     """
     Given an image, return the 128-dimension face encoding for each face in the image.
 
@@ -206,7 +206,7 @@ def face_encodings(face_image, known_face_locations=None, num_jitters=1):
     :param num_jitters: How many times to re-sample the face when calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
     :return: A list of 128-dimensional face encodings (one for each face in the image)
     """
-    raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model="small")
+    raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model)
     return [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for raw_landmark_set in raw_landmarks]
 
 
