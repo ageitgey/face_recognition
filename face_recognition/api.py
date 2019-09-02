1239c4d566e4e0dc49e4c9f51dabe173dd7e1736
Tejas Shah <tejashah88@gmail.com>
2019-09-02 14:11:26 -0700

    allowed face_encodings to accept either 'large' or 'small' model

--- face_recognition/api.py
+++ face_recognition/api.py
@@ -200,16 +200,17 @@ def face_landmarks(face_image, face_locations=None, model="large"):
         raise ValueError("Invalid landmarks model type. Supported models are ['small', 'large'].")
 
 
-def face_encodings(face_image, known_face_locations=None, num_jitters=1):
+def face_encodings(face_image, known_face_locations=None, num_jitters=1, model="small"):
     """
     Given an image, return the 128-dimension face encoding for each face in the image.
 
     :param face_image: The image that contains one or more faces
     :param known_face_locations: Optional - the bounding boxes of each face if you already know them.
     :param num_jitters: How many times to re-sample the face when calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
+    :param model: Optional - which model to use. "large" (default) or "small" which only returns 5 points but is faster.
     :return: A list of 128-dimensional face encodings (one for each face in the image)
     """
-    raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model="small")
+    raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model)
     return [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for raw_landmark_set in raw_landmarks]
 
 
