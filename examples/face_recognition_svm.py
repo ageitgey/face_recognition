03a09eea5d5e4e29b9ec866f626b1e475683371e
Ankitkumar Singh <17850047+Anki7singh@users.noreply.github.com>
2019-10-10 20:39:55 +0530

    Update face_recognition_svm.py

--- examples/face_recognition_svm.py
+++ examples/face_recognition_svm.py
@@ -37,17 +37,19 @@ import os
 encodings = []
 names = []
 
+#Model path 
+model_save_path="trained_svn_model.clf"
 # Training directory
-train_dir = os.listdir('/train_dir/')
+train_dir = os.listdir('train_dir/')
 
 # Loop through each person in the training directory
 for person in train_dir:
-    pix = os.listdir("/train_dir/" + person)
+    pix = os.listdir("train_dir/" + person)
 
     # Loop through each training image for the current person
     for person_img in pix:
         # Get the face encodings for the face in each image file
-        face = face_recognition.load_image_file("/train_dir/" + person + "/" + person_img)
+        face = face_recognition.load_image_file("train_dir/" + person + "/" + person_img)
         face_bounding_boxes = face_recognition.face_locations(face)
 
         #If training image contains none or more than faces, print an error message and exit
@@ -60,12 +62,20 @@ for person in train_dir:
             encodings.append(face_enc)
             names.append(person)
 
-# Create and train the SVC classifier
+#Train the SVC classifier
 clf = svm.SVC(gamma='scale')
 clf.fit(encodings,names)
 
+#Saved Model
+if model_save_path is not None:
+        with open(model_save_path, 'ab') as f:
+            pickle.dump(clf, f)
+
+# Load the model
+with open(model_save_path, 'rb') as f:
+    svm_clf = pickle.load(f)
 # Load the test image with unknown faces into a numpy array
-test_image = face_recognition.load_image_file('test_image.jpg')
+test_image = face_recognition.load_image_file('test_img.jpg')
 
 # Find all the faces in the test image using the default HOG-based model
 face_locations = face_recognition.face_locations(test_image)
@@ -76,5 +86,6 @@ print("Number of faces detected: ", no)
 print("Found:")
 for i in range(no):
     test_image_enc = face_recognition.face_encodings(test_image)[i]
-    name = clf.predict([test_image_enc])
+    name = svm_clf.predict([test_image_enc])
     print(*name)
+            
