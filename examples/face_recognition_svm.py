99e93d8c8feba6a3ac5628a1e216a2c1ed96c69f
Anand Baburajan <anandbaburajan@gmail.com>
2019-12-01 15:59:41 +0530

    Skip a pic if it has none or more than one face

--- examples/face_recognition_svm.py
+++ examples/face_recognition_svm.py
@@ -50,11 +50,8 @@ for person in train_dir:
         face = face_recognition.load_image_file("/train_dir/" + person + "/" + person_img)
         face_bounding_boxes = face_recognition.face_locations(face)
 
-        #If training image contains none or more than faces, print an error message and exit
-        if len(face_bounding_boxes) != 1:
-            print(person + "/" + person_img + " contains none or more than one faces and can't be used for training.")
-            exit()
-        else:
+        #If training image contains exactly one face
+        if len(face_bounding_boxes) == 1:
             face_enc = face_recognition.face_encodings(face)[0]
             # Add face encoding for current image with corresponding label (name) to the training data
             encodings.append(face_enc)
