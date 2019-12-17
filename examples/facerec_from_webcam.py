ff5a13a4694a144cd6e7ddf25f9ead13b425c501
perinm <lucasperinm@gmail.com>
2019-12-17 16:34:56 -0300

    added live face rec and saving features

--- examples/facerec_from_webcam.py
+++ examples/facerec_from_webcam.py
@@ -1,9 +1,14 @@
 import face_recognition
 import cv2
 import numpy as np
+from math import sin, cos, radians
+import pickle
+import os
 
-# This is a super simple (but slow) example of running face recognition on live video from your webcam.
-# There's a second example that's a little more complicated but runs faster.
+# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
+# other example, but it includes some basic performance tweaks to make things run a lot faster:
+#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
+#   2. Only detect faces in every other frame of video.
 
 # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
 # OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
@@ -30,34 +35,76 @@ known_face_names = [
     "Joe Biden"
 ]
 
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+ArquivoEncoding = "Codings"
+ArquivoNome = "Nomes"
+
+# Test if there are already files to load
+# In case there is, it will just load
+# Otherwise, a base will be created, converted to bits and loaded into the script
+
+if not(os.path.isfile("Codings")):
+    with open(ArquivoEncoding, 'wb') as fp:
+        pickle.dump(known_face_encodings, fp)
+with open (ArquivoEncoding, 'rb') as fp:
+    known_face_encodings = pickle.load(fp)
+
+if not(os.path.isfile("Nomes")):
+    with open(ArquivoNome, 'wb') as fp:
+        pickle.dump(known_face_names, fp)
+with open (ArquivoNome, 'rb') as fp:
+    known_face_names = pickle.load(fp)
+
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_frame = frame[:, :, ::-1]
+    rgb_small_frame = small_frame[:, :, ::-1]
 
-    # Find all the faces and face enqcodings in the frame of video
-    face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-    # Loop through each face in this frame of video
-    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
 
-        name = "Unknown"
+            # # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
 
-        # If a match was found in known_face_encodings, just use the first one.
-        # if True in matches:
-        #     first_match_index = matches.index(True)
-        #     name = known_face_names[first_match_index]
+            # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
 
-        # Or instead, use the known face with the smallest distance to the new face
-        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-        best_match_index = np.argmin(face_distances)
-        if matches[best_match_index]:
-            name = known_face_names[best_match_index]
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
@@ -71,9 +118,36 @@ while True:
     cv2.imshow('Video', frame)
 
     # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(33) & 0xFF == ord('q'):
         break
 
+    # Press 'c' to capture face and assing a name to it
+    if cv2.waitKey(33) & 0xFF == ord('c'):
+        # Capture the picture
+        ret, frame = video_capture.read()
+        frameRGB = frame[:,:,::-1]
+        # Recognize and store face's measures
+        known_face_encodings.append(face_recognition.face_encodings(frameRGB)[0])
+        # Assign a name to the captured face
+        nome = input("Type captured face's name: ")
+        known_face_names.append(nome)
+        # Save the image for future use
+        cv2.imwrite('opencv '+nome+'.png', frame)
+
+    # Press 's' to save everything into bits
+    if cv2.waitKey(33) & 0xFF == ord('s'):
+        if bool(input("Do you wish to save all the names and encodings? (1|0):\n")):
+            with open(ArquivoEncoding, 'wb') as fp:
+                pickle.dump(known_face_encodings, fp)
+            with open (ArquivoEncoding, 'rb') as fp:
+                known_face_encodings = pickle.load(fp)
+            with open(ArquivoNome, 'wb') as fp:
+                pickle.dump(known_face_names, fp)
+            with open (ArquivoNome, 'rb') as fp:
+                known_face_names = pickle.load(fp)
+            print("Names' and encodings' lists converted in binary and saved!")
+
 # Release handle to the webcam
+
 video_capture.release()
 cv2.destroyAllWindows()
