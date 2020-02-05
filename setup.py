2f50124ac209a31e4913195d27a94029a8719b30
Santiago Castro <sacastro@umich.edu>
2020-02-04 20:26:50 -0500

    Drop support for Python 3.4 and add 3.8

--- setup.py
+++ setup.py
@@ -55,10 +55,10 @@ setup(
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     test_suite='tests',
     tests_require=test_requirements
