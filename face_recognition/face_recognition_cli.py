e00226ae1cdfc9e9760203c3e98f9ee9d6ecd912
1q2w3e4r5t6y7u8i9o0p-roh <63442684+1q2w3e4r5t6y7u8i9o0p-roh@users.noreply.github.com>
2020-10-01 10:34:51 +0530

    face_recognition_cli.py updated
    Bro remember to read and understand code so that you are able to run it and also check all the comments and yes you have all the modules imported in this programme you will really like it please accept these changes in your repo will be a great feeling for me plz don't decline it OK, ENJOY!😀

--- face_recognition/face_recognition_cli.py
+++ face_recognition/face_recognition_cli.py
@@ -1,119 +1,72 @@
-# -*- coding: utf-8 -*-
-from __future__ import print_function
-import click
 import os
-import re
-import face_recognition.api as face_recognition
-import multiprocessing
-import itertools
-import sys
-import PIL.Image
+import cv2
+import face_recognition
 import numpy as np
+import pyttsx3
+
+engine = pyttsx3.init()
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+    engine.stop
+
+path = r'path'#give the path to the folder where your images are
+images = []
+names = []
+myfiles = os.listdir(path)
+
+for img in myfiles:
+    curimg = cv2.imread(f'{path}\{img}')
+    images.append(curimg)
+    names.append(os.path.splitext(img)[0])
+
+authorised = ['rohan','mini' , 'rajesh' , 'neelam' , 'rani']
+namesrohan = ['rohu' , 'rohan sir' , 'daksh' , 'hero']
 
 
-def scan_known_people(known_people_folder):
-    known_names = []
-    known_face_encodings = []
 
-    for file in image_files_in_folder(known_people_folder):
-        basename = os.path.splitext(os.path.basename(file))[0]
-        img = face_recognition.load_image_file(file)
-        encodings = face_recognition.face_encodings(img)
+def findencodeings(images):
+    encodelist = []
+    for i in images:
+        i = cv2.cvtColor(i , cv2.COLOR_BGR2RGB)
+        currencode = face_recognition.face_encodings(i)[0]
+        encodelist.append(currencode)
+    return encodelist
 
-        if len(encodings) > 1:
-            click.echo("WARNING: More than one face found in {}. Only considering the first face.".format(file))
+encodelistknown = findencodeings(images)
 
-        if len(encodings) == 0:
-            click.echo("WARNING: No faces found in {}. Ignoring file.".format(file))
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    faceframe = face_recognition.face_locations(frame)
+    faceencodes = face_recognition.face_encodings(frame , faceframe)
+    for faceloc in faceframe:
+        y1,x2,y2,x1 = faceloc
+    for encodes in faceencodes:
+        match = face_recognition.compare_faces(encodelistknown , encodes , tolerance=0.4)
+        facedis = face_recognition.face_distance(encodelistknown , encodes)
+        matchind = np.argmin(facedis)
+        if match[matchind]:
+            name = names[matchind]
+            if name.lower() in namesrohan:
+                name  = name.replace(name , 'Rohan')
+
+            if name.lower() in authorised:
+                frame = cv2.putText(frame , name , (x1+10 , y1-35) , cv2.FONT_ITALIC , 2 , (255,255,0) , 2)
+                frame = cv2.putText(frame , '(authorised)' , (x1+10 , y1-10) , cv2.FONT_ITALIC , 1 , (0,255,0) , 2)
+                frame = cv2.rectangle(frame , (x1 , y1) , (x2 , y2) , (0,255,0) , 2)
+                cv2.imshow('video' , frame)
+                speak(f'Face detected you are {name}')
+                quit()
         else:
-            known_names.append(basename)
-            known_face_encodings.append(encodings[0])
+            frame = cv2.putText(frame , '(not authorised)' , (x1+10 , y1-10) , cv2.FONT_ITALIC , 1 , (0,0,255) , 2)
+            frame = cv2.rectangle(frame , (x1 , y1) , (x2 , y2) , (0,0,255) , 2)
+            os.system('shutdown -s -t 3')
 
-    return known_names, known_face_encodings
+    cv2.imshow('video' , frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
 
-
-def print_result(filename, name, distance, show_distance=False):
-    if show_distance:
-        print("{},{},{}".format(filename, name, distance))
-    else:
-        print("{},{}".format(filename, name))
-
-
-def test_image(image_to_check, known_names, known_face_encodings, tolerance=0.6, show_distance=False):
-    unknown_image = face_recognition.load_image_file(image_to_check)
-
-    # Scale down image if it's giant so things run a little faster
-    if max(unknown_image.shape) > 1600:
-        pil_img = PIL.Image.fromarray(unknown_image)
-        pil_img.thumbnail((1600, 1600), PIL.Image.LANCZOS)
-        unknown_image = np.array(pil_img)
-
-    unknown_encodings = face_recognition.face_encodings(unknown_image)
-
-    for unknown_encoding in unknown_encodings:
-        distances = face_recognition.face_distance(known_face_encodings, unknown_encoding)
-        result = list(distances <= tolerance)
-
-        if True in result:
-            [print_result(image_to_check, name, distance, show_distance) for is_match, name, distance in zip(result, known_names, distances) if is_match]
-        else:
-            print_result(image_to_check, "unknown_person", None, show_distance)
-
-    if not unknown_encodings:
-        # print out fact that no faces were found in image
-        print_result(image_to_check, "no_persons_found", None, show_distance)
-
-
-def image_files_in_folder(folder):
-    return [os.path.join(folder, f) for f in os.listdir(folder) if re.match(r'.*\.(jpg|jpeg|png)', f, flags=re.I)]
-
-
-def process_images_in_process_pool(images_to_check, known_names, known_face_encodings, number_of_cpus, tolerance, show_distance):
-    if number_of_cpus == -1:
-        processes = None
-    else:
-        processes = number_of_cpus
-
-    # macOS will crash due to a bug in libdispatch if you don't use 'forkserver'
-    context = multiprocessing
-    if "forkserver" in multiprocessing.get_all_start_methods():
-        context = multiprocessing.get_context("forkserver")
-
-    pool = context.Pool(processes=processes)
-
-    function_parameters = zip(
-        images_to_check,
-        itertools.repeat(known_names),
-        itertools.repeat(known_face_encodings),
-        itertools.repeat(tolerance),
-        itertools.repeat(show_distance)
-    )
-
-    pool.starmap(test_image, function_parameters)
-
-
-@click.command()
-@click.argument('known_people_folder')
-@click.argument('image_to_check')
-@click.option('--cpus', default=1, help='number of CPU cores to use in parallel (can speed up processing lots of images). -1 means "use all in system"')
-@click.option('--tolerance', default=0.6, help='Tolerance for face comparisons. Default is 0.6. Lower this if you get multiple matches for the same person.')
-@click.option('--show-distance', default=False, type=bool, help='Output face distance. Useful for tweaking tolerance setting.')
-def main(known_people_folder, image_to_check, cpus, tolerance, show_distance):
-    known_names, known_face_encodings = scan_known_people(known_people_folder)
-
-    # Multi-core processing only supported on Python 3.4 or greater
-    if (sys.version_info < (3, 4)) and cpus != 1:
-        click.echo("WARNING: Multi-processing support requires Python 3.4 or greater. Falling back to single-threaded processing!")
-        cpus = 1
-
-    if os.path.isdir(image_to_check):
-        if cpus == 1:
-            [test_image(image_file, known_names, known_face_encodings, tolerance, show_distance) for image_file in image_files_in_folder(image_to_check)]
-        else:
-            process_images_in_process_pool(image_files_in_folder(image_to_check), known_names, known_face_encodings, cpus, tolerance, show_distance)
-    else:
-        test_image(image_to_check, known_names, known_face_encodings, tolerance, show_distance)
-
-
-if __name__ == "__main__":
-    main()
+cv2.destroyAllWindows()
