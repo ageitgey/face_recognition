0d9102b1f1ceb0c52dfef80d93f2a9684a61dd92
Utkrist Arvind <56083956+utkrist11@users.noreply.github.com>
2021-08-18 11:18:31 +0530

    Update __init__.py

--- face_recognition/__init__.py
+++ face_recognition/__init__.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-
-__author__ = """Adam Geitgey"""
-__email__ = 'ageitgey@gmail.com'
+__author__ = """utkrist varma"""
 __version__ = '1.4.0'
 
-from .api import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
+from .api batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance ,import load_image_file, face_locations
