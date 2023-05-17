8d91f4cd1a5685c780578cc760b1364acbec0eb9
Dayoung Kang <93754504+riverallzero@users.noreply.github.com>
2023-05-17 18:20:36 +0900

    Update facerec_from_webcam_faster.py

--- examples/facerec_from_webcam_faster.py
+++ examples/facerec_from_webcam_faster.py
@@ -48,11 +48,11 @@ while True:
         small_frame = cv2.resize(frame, (0, 0), fx=1, fy=1)
 
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+        rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
 
         # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_frame)
-        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
